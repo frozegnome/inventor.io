37e45e6fede8ade396c9357dfd2aa075d3bbd036
pcaron11 <pcaron@att.net>
2015-11-20 14:55:55 -0500

    Add light animations to content wells and begin taking care of code documentation
    Really should have been commenting all along, remember for the future

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,20 +5,24 @@ angular.module('inventorioApp')
   function ($scope, $state, socket, Auth, Inventory) {
     var vm = this;
 
+    // Declare ViewModel properties
     vm.itemListActive = [];
     vm.itemListInactive = [];
     vm.itemToAdd = {};
     vm.tempItem = Inventory.getServiceItem();
 
+    // Check if a user is logged in
     $scope.isLoggedIn = Auth.isLoggedIn;
 
     activate();
 
+    // Transfers temporary item to service for transferring views
     vm.transferEditView = function(i) {
       Inventory.setServiceItem(i);
       $state.go('edit');
     };
 
+    // Calls GET service to remove item from interface
     vm.addNewItem = function() {
       Inventory.postItem(vm.itemToAdd)
       .success(function(response) {
@@ -31,6 +35,7 @@ angular.module('inventorioApp')
       });
     };
 
+    // Calls PUT service to remove item from interface
     vm.editCurrentItem = function() {
       Inventory.putItem(vm.tempItem)
       .success(function(response) {
@@ -43,6 +48,7 @@ angular.module('inventorioApp')
       });
     };
 
+    // Calls DELETE service to remove item from interface
     vm.deleteCurrentItem = function() {
       var confirmDelete = confirm('Are you sure you want to delete ' + vm.tempItem.name + '?');
       if(confirmDelete) {
@@ -59,6 +65,7 @@ angular.module('inventorioApp')
       }
     };
 
+    // Instantiate the main page
     function activate() {
       Inventory.getItems()
       .success(function(response) {
